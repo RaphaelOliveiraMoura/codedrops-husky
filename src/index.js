e8c17bf674fdb4affcd528e5c4d6e592310ab659
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2020-01-19 16:11:31 -0300

    Add cors configurations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(3333, console.log('Server running...'));
